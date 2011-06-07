8834ad7c77ad4729f9a7961443d1d74570e11841
Anders Janmyr <anders@janmyr.com>
2011-06-07 13:07:33 +0200

    Added more routes

--- lib/neo-viz.rb
+++ lib/neo-viz.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 
+require 'neo4j'
+
 module NeoViz
   class App < Sinatra::Base
     configure(:development) do
@@ -12,5 +14,49 @@ module NeoViz
       p request.env
       request.env.inspect
     end
+
+    get '/node-count' do
+      "Number of nodes before index: #{Neo4j.management.get_number_of_node_ids_in_use}"
+    end
+
+    get '/root' do
+      Neo4j.ref_node.inspect
+    end
+
+    get '/tree' do
+      Neo4j.ref_node.rels.map { |rel| rel.end_node.attributes }.inspect
+    end
+
+    get '/nodes' do
+      node_to_hash(Neo4j.ref_node, 1).to_json
+    end
+
+    get '/nodes/:id' do |id|
+      node = Neo4j::Node._load(id)
+      node_to_hash(node, 1).to_json
+    end
+
+    private
+    def node_to_hash(node, depth=1)
+      node_hash = only_node(node)
+      return node_hash if depth == 0
+      rels = node._rels.map do |rel|
+        relation(node, rel, depth-1)
+      end
+      node_hash[:rels] = rels
+      node_hash
+    end
+
+    def only_node(node) 
+      node.props
+    end
+
+    def relation(node, rel, depth)
+      p rel
+      rel.props.merge(:rel_type => rel.rel_type,
+                      :direction => (node == rel._end_node ? :incoming : :outgoing),
+                      :end_node => node_to_hash(rel._other_node(node), depth))
+    end
   end
+
 end
